d7e4b0e65ade9865db02b583af119d21342bb872
ANNASBlackHat <annas685@gmail.com>
2017-03-04 01:47:01 +0700

    init commit

--- app/src/main/java/com/annasblackhat/daggerprovidesnetworking/ui/MainActivity.java
+++ app/src/main/java/com/annasblackhat/daggerprovidesnetworking/ui/MainActivity.java
@@ -2,17 +2,27 @@ package com.annasblackhat.daggerprovidesnetworking.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.annasblackhat.daggerprovidesnetworking.R;
 import com.annasblackhat.daggerprovidesnetworking.dependency.ContextModule;
 import com.annasblackhat.daggerprovidesnetworking.dependency.DaggerGitComponent;
 import com.annasblackhat.daggerprovidesnetworking.dependency.GitComponent;
+import com.annasblackhat.daggerprovidesnetworking.model.Repository;
 import com.annasblackhat.daggerprovidesnetworking.network.GithubService;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Inject
     Picasso picasso;
@@ -30,7 +40,23 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         component.injectMainActivity(this);
 
-        System.out.println("Picasso IS NULL : "+String.valueOf(picasso == null));
-        System.out.println("Service IS NULL : "+String.valueOf(githubService == null));
+        githubService.getRepositories().enqueue(new Callback<List<Repository>>() {
+            @Override
+            public void onResponse(Call<List<Repository>> call, Response<List<Repository>> response) {
+                if(response.isSuccessful()){
+                    List<Repository> repositories = response.body();
+                    for(Repository repo : repositories){
+                        Log.d(TAG, "onResponse: "+repo.getName());
+                    }
+                }else{
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Repository>> call, Throwable t) {
+
+            }
+        });
     }
 }
